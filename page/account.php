aba45d6af6760b93197302a5d370e75d1585e977
Firman Ardiansyah <41628497+heifir@users.noreply.github.com>
2018-11-06 22:31:15 +0700

    Delete account.php

--- page/account.php
+++
@@ -1,4 +0,0 @@
-<div class="mybody">
-    <img src="https://cdn.dribbble.com/users/486498/screenshots/4757900/24_3.jpg" alt="" width="100%" height="100%">
-    <center style="font-family: 'Roboto Condensed', sans-serif;"><h3>Get in touch</h3></center>
-</div>
